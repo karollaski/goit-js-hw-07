73e11329de1236e87e8199f7fd346b88bbbac0e5
karollaski <karol.laski994@gmail.com>
2023-02-25 18:04:56 +0100

    finished task-02
    created a gallery by SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryWrap = document.querySelector(".gallery");
+const galleryCollection = createGallery(galleryItems);
+
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>`
+    )
+    .join("");
+}
+
+galleryWrap.insertAdjacentHTML("beforeend", galleryCollection);
+
+// ------------------funkcja do otwierania okna modalnego------------------
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionsDelay: 250,
+});
